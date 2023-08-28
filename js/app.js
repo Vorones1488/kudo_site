36433ce55cab8484f425ba6ea29e054846093141
Mihail <gmy2525@gmsil.com>
2023-08-28 21:47:10 +0300

    add_name

--- js/app.js
+++ js/app.js
@@ -1,21 +1,10 @@
-function app(){
-//ввод данных
-    //let name = prompt("введите имя")
-    let number = parseInt(document.getElementById('inp').value) 
-    let result = NaN
-//логика
-if(number == NaN) {
-    alert('Ошибка! вы ввели текст введите пожалуйста число!')
-    }
-if(number == 0){
-        document.getElementById('inform').innerText = 'квадрат нуля равен нулю'
-    }  
-    else {
-        result = number * number
-        document.getElementById('results').innerText = result
-    }
-    
-
-//вывод
-
+   
+function name1(){
+    let first_name = prompt('Введите ваше имя')
+    let second_name = prompt ('введите вашу фамилию')
+    //логика
+    let yur_name = `${first_name} ${second_name}`
+    document.getElementById("names").innerText = yur_name
+ 
 }
+
